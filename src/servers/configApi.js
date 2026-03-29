8f6372eec65e3336e87b81cae0ad733a21def58a
stefanynovais <stefanynovaisnasc@gmail.com>
2026-03-29 13:56:16 -0300

    Mudando o config api

--- src/servers/configApi.js
+++ src/servers/configApi.js
@@ -1,3 +1,5 @@
 // URL gerada pelo local tunnel
 // centraliza a URL da API 
-export const API_URL = " https://cool-coats-feel.loca.lt";
+// export const API_URL = "https://cool-coats-feel.loca.lt";
+
+export const API_URL = "http://localhost:3000";
